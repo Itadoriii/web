ce42ea0cefc0f37ad15ed36c85656947ddb32ce4
sebas <scastroc@utem.cl>
2025-03-03 22:14:48 -0300

    Primer commit

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+const PORT = 3000;
+
+// Servir archivos estáticos desde la carpeta src
+app.use(express.static(path.join(__dirname, "src")));
+
+// Ruta principal que sirve index.html
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "src", "index.html"));
+});
+
+// Iniciar servidor
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
